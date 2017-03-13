2682c2cd472d7bd5f12a94a5b8b6e9670321205d
Garodan <can.h.kalkan@gmail.com>
2017-03-13 12:06:12 +0100

    Änderung
    head.html für CSS und Skripte die in jeden Template gebraucht werden
    header.html navbar und co
    ansonsten kleine änderungen. noch nicht fertig

--- treffenerstellen.php
+++ treffenerstellen.php
@@ -1,84 +1,21 @@
 ﻿<!DOCTYPE html>
-<html lang="de">
+<html ng-app>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>LernTreff erstellen</title>
-
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/trefflist.css" rel="stylesheet">
-    <link href="css/navbar.css" rel="stylesheet">
-    <!--link href="css/buttons.css" rel="stylesheet"-->
-
-
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+  <title>LernTreff erstellen</title>
+  <link href="css/trefflist.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.1/angular.min.js"></script>
 
 </head>
 <body>
-  <header>
-      <div class="navbar-wrapper">
-          <div class="container-fluid">
-              <nav class="navbar navbar-fixed-top">
-                  <div class="container">
-                      <div class="navbar-header">
-                          <a class="navbar-brand" href="startseite.html">LernTreff</a>
-                      </div>
-                      <div id="navbar" class="navbar-collapse collapse">
-                          <ul class="nav navbar-nav">
-                            <li class=" dropdown">
-                                <a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Forum <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li class=" dropdown">
-                                        <a href="forumerstellen.php">Forumsbeitrag erstellen</a>
-                                    </li>
-                                    <li><a href="forumliste.php">Forumsbeiträge</a></li>
-                                </ul>
-                            </li>
-                              <li class=" dropdown">
-                                  <a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Treffen <span class="caret"></span></a>
-                                  <ul class="dropdown-menu">
-                                      <li class=" dropdown">
-                                          <a href="treffenerstellen.php">Neues Treffen erstellen</a>
-                                      </li>
-                                      <li><a href="treffenliste.php">Liste der Treffen</a></li>
-                                  </ul>
-                              </li>
-                              <li class="active"><a href="kalender.html">Kalender</a></li>
-                          </ul>
-                          <ul class="nav navbar-nav pull-right">
-                              <li class=" dropdown">
-                                  <a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Eingeloggt als  <span class="caret"></span></a>
-                                  <ul class="dropdown-menu">
+  <ng-include src="'header.html'"></ng-include>
 
-                                      <li><a href="#">Mein Profil</a></li>
-                                  </ul>
-                              </li>
-                              <li class=""><a href="index.html">Logout</a></li>
-                          </ul>
-                      </div>
-                  </div>
-              </nav>
-          </div>
-      </div>
-  </header>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css">
-    </head>
-    <div ng-app="myapp2" ng-controller="forumcontroller">
+    <!-- Treffen erstellen FORM -->
+    <div>
     <form class="form-horizontal" >
         <fieldset>
 
-            <!-- Form Name -->
-            <!--<legend>Neuer LernTreff</legend>-->
+
 
             <!-- Text input-->
             <div class="form-group">
