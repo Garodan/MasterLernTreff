dd1bdf70448c6bcd6c1dab14d2369ff7ca3cc8a8
MrBeats <mschendel12@gmail.com>
2017-03-14 09:27:57 +0100

    kp

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 angular
     .module ('lerntreff', [])
     .controller('treffenInsert', function($scope, $http){
-    $scope.treffenInsert = function(){
-        $http.post(
-            "php/insertliste.php",
-            {'ueberschrift':$scope.ueberschrift, 'studiengang':$scope.studiengang, 'semester':$scope.semester, 'modul':$scope.modul, 'ort':$scope.ort, 'datum':$scope.datum, 'zeit':$scope.zeit, 'beschreibung':$scope.beschreibung}
-        ).success(function(data){
+    $scope.treffenInsert=function(){
+        $http.post("php/insertliste.php",{'ueberschrift':$scope.ueberschrift, 'studiengang':$scope.studiengang, 'semester':$scope.semester, 'modul':$scope.modul, 'ort':$scope.ort, 'datum':$scope.datum, 'zeit':$scope.zeit, 'beschreibung':$scope.beschreibung}
+        ).success(function(data, status, headers, config){
+            console.log('Erfolgreich');
             alert(data);
             $scope.ueberschrift = null;
             $scope.studiengang = null;
